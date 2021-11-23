d8bf2f6e56d4c619c69e816d12dd6b1c1bf6035b
hwb15 <47414652+hwb15@users.noreply.github.com>
2021-11-23 17:21:49 +0000

    Interacting with my sol contract I deployed on Rinkeby

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react";
+import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import abi from "./utils/GmPortal.json";
 
 const App = () => {
   // Store user's wallet
   const [currentWallet, setCurrentWallet] = React.useState("");
+  const [totalGms, setTotalGms] = useState(0);
+  const contractAddress = "0x3c731fDF135c73736B90Da2d78964922ddCCFb91";
+  const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -23,6 +28,7 @@ const App = () => {
         const account = accounts[0];
         console.log(`Found an authorized acc`, account);
         setCurrentWallet(account);
+        getTotalGms();
       } else {
         console.log(`No accounts found`);
       }
@@ -52,11 +58,65 @@ const App = () => {
     }
   };
 
+  const getTotalGms = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const gmPortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        const totalGms = await gmPortalContract.getTotalGms();
+        setTotalGms(totalGms.toNumber());
+      } else {
+        console.log(`No eth obj`);
+      }
+    } catch (error) {
+      console.log(`Error`, error);
+    }
+  };
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
 
-  const gm = () => {};
+  const gm = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const gmPortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let gmCount = await gmPortalContract.getTotalGms();
+        console.log(`Retrieved total gms, ${gmCount.toNumber()}`);
+
+        // write to contract
+        const gmTxn = await gmPortalContract.gm();
+        console.log(`Mining... ${gmTxn.hash}`);
+
+        await gmTxn.wait();
+        console.log(`Mined -- ${gmTxn.hash}`);
+
+        gmCount = await gmPortalContract.getTotalGms();
+        console.log(`Retrieved total gms, ${gmCount.toNumber()}`);
+      } else {
+        console.log(`No ethereum object`);
+      }
+    } catch (error) {
+      console.log(`Error`, error);
+    }
+  };
 
   return (
     <div className="mainContainer">
@@ -66,6 +126,9 @@ const App = () => {
       </div>
 
       <div className="action-container">
+        <div className="gm-counter">
+          I have been wished gm by {totalGms} people!
+        </div>
         <div className="bio">
           Wish me a gm by interacting with my first solidity contract deployed
           on rinkerby! wgmi!

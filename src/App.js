8324cd1d48fc711ef7274a4ef4f79982178207af
hwb15 <47414652+hwb15@users.noreply.github.com>
2021-11-23 14:33:49 +0000

    Added functionality to check for MM wallet and also connect / ask to auth to a wallet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect } from "react";
 import "./App.css";
 
-export default function App() {
+const App = () => {
+  // Store user's wallet
+  const [currentWallet, setCurrentWallet] = React.useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log(`Make sure you have installed Metamask!`);
+        return;
+      } else {
+        console.log(`We have the ethereum object`, ethereum);
+      }
+
+      // Check authorisation for user's wallet
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log(`Found an authorized acc`, account);
+        setCurrentWallet(account);
+      } else {
+        console.log(`No accounts found`);
+      }
+    } catch (error) {
+      console.log(`Error`, error);
+    }
+  };
+
+  // Connect to ethereum wallet
+  const connectToWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log(`Make sure you have installed Metamask!`);
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log(`Connected ${accounts[0]}`);
+      setCurrentWallet(accounts[0]);
+    } catch (error) {
+      console.log(`Error`, error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   const gm = () => {};
 
   return (
@@ -21,7 +74,15 @@ export default function App() {
         <button className="waveButton" onClick={gm}>
           gm!
         </button>
+
+        {!currentWallet && (
+          <button className="connect-button" onClick={connectToWallet}>
+            Connect Wallet!
+          </button>
+        )}
       </div>
     </div>
   );
-}
+};
+
+export default App;
